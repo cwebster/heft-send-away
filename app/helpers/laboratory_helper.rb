3dcf4807c722cbefa021691331e646f6f83d346d
Craig Webster <craig.webster@mac.com>
2016-02-08 17:49:10 +0000

    Update of print form

--- app/helpers/laboratory_helper.rb
+++ app/helpers/laboratory_helper.rb
@@ -1,2 +1,14 @@
 module LaboratoryHelper
+
+	def ukas_status_html(status)
+		response = ''
+		if status.nil?
+			response = "Yes/No"
+		elsif status = 'Yes'
+			response = 'Yes'
+		else
+			response = 'No'
+		end
+    return response
+	end
 end
