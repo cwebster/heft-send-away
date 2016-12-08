0cc1b9db65beba82eb841fd4cd144ce45d84915f
Craig Webster <craig.webster@mac.com>
2016-12-08 10:45:33 +0000

    change in api serialzer for lab test

--- app/serializers/laboratory_test_serializer.rb
+++ app/serializers/laboratory_test_serializer.rb
@@ -1,4 +1,18 @@
 class LaboratoryTestSerializer < BaseSerializer
 	attribute :analyte_name
+	attribute :eqa_status
+	attribute :turnaround_time
+	attribute :ukas_status
+	attribute :methodology
+	attribute :reference_range
+	attribute :sample_type
+	attribute :cost_of_test
+	attribute :uncertainty_of_measurement
+	attribute :eqa_provider
+	attribute :eqa_performance
+	attribute :sample_volume
+	attribute :special_collection_requirements
+	attribute :ukas_reference_number
+	attribute :date_test_information_updated
 	has_one :laboratory
 end
